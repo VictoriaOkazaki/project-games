8efe998272fe85758f813d55d6185224334c5fd8
VictoriaOkazaki <victoria.timein@gmail.com>
2024-05-21 15:45:58 +0400

    init

---
+++ js/main.js
@@ -0,0 +1,81 @@
+let choosedGame = ''
+
+function createSelect() {
+    const dropdownBtn = document.querySelector('.dropdown__btn')
+    const dropdownMenu = document.querySelector('.dropdown__list')
+    const items = document.querySelectorAll('.dropdown__list-item')
+    const selectedItem = document.getElementById('dropdown-selected')
+    const select = document.getElementById("dropdown");
+    const points = document.querySelector('.points-cont');
+
+    function selectGame(game) {
+        console.log("Select on change", game.name);
+        const index = GAMES_LIST.findIndex((el) => el.id === game.id);
+        setSliderActiveIndex(index);
+
+        selectedItem.innerText = game.name
+        choosedGame = game.name
+        items.forEach((item) => item.classList.remove('active'))
+
+        success.style.display = 'none';
+        circle.classList.remove('success');
+        circle.classList.remove('animation');
+        circle.style.animation = '';
+
+    }
+
+    GAMES_LIST.forEach((game) => {
+        const option = document.createElement("a");
+        option.href = `#game${game.id}`;
+        option.classList.add('dropdown__list-item')
+        option.innerHTML = game.name;
+        option.onclick = (e) => {
+            selectGame(game);
+            e.target.classList.add('active');
+            closeMenu()
+        }
+        select.appendChild(option);
+
+        const point = document.createElement('div');
+        point.classList.add('point');
+        points.appendChild(point);
+        point.onclick = (e) => {
+            selectGame(game);
+            points.childNodes.forEach(child => { child.classList.remove('point-active') })
+            e.target.classList.add('point-active')
+        }
+    });
+
+    dropdownBtn.addEventListener('click', toggleMenu)
+
+    function toggleMenu() {
+        dropdownMenu.classList.toggle('show')
+    }
+    function closeMenu() {
+        dropdownMenu.classList.remove('show')
+    }
+}
+
+createSelect()
+startSlider()
+
+const bigBtn = document.querySelector('.big-btn')
+const circle = document.querySelector('.circle')
+const success = document.querySelector('.success-text')
+circle.addEventListener('animationend', function (e) {
+    circle.classList.remove('animation');
+    circle.style.animation = '';
+    if (prevGame !== choosedGame) return;
+    success.style.display = 'block';
+    circle.classList.add('success');
+})
+let prevGame = ''
+bigBtn.onclick = () => {
+    if (choosedGame === '') return;
+    if (prevGame === choosedGame) return;
+
+    circle.classList.add('animation');
+    circle.style.animation = 'spin 2s linear';
+
+    prevGame = choosedGame
+}
---
+++ js/slider.js
@@ -0,0 +1,87 @@
+const GAMES_LIST = [
+    { id: '1', name: "Counter Strike 2", img: "img/img-1.jpg" },
+    { id: '2', name: "Dota 2", img: "img/img-2.jpg" },
+    { id: '3', name: "Assasin's Creed 2", img: "img/img-3.jpg" },
+    { id: '4', name: "Counter Strike 2", img: "img/img-1.jpg" },
+    { id: '5', name: "Dota 2", img: "img/img-2.jpg" },
+    { id: '6', name: "Assasin's Creed 2", img: "img/img-3.jpg" },
+    { id: '7', name: "Counter Strike 2", img: "img/img-1.jpg" },
+    { id: '8', name: "Dota 2", img: "img/img-2.jpg" },
+    { id: '9', name: "Assasin's Creed 2", img: "img/img-3.jpg" },
+]
+
+function getGameImage(index) {
+    return GAMES_LIST[index].img;
+}
+
+function setActivePoint(index) {
+    const points = document.querySelector('.points-cont');
+    points.childNodes.forEach(child => { child.classList.remove('point-active') })
+    points.childNodes[index].classList.add('point-active')
+}
+
+let curIndex = 0;
+
+function startSlider() {
+    const img1 = document.getElementById("slider-img-1");
+    const img2 = document.getElementById("slider-img-2");
+
+    setActivePoint(curIndex);
+    img1.src = getGameImage(curIndex);
+    prepareNextImage(img1, img2);
+}
+
+let swapTm;
+function prepareNextImage(curImg, nextImg) {
+    const nextIndex = (curIndex + 1) % GAMES_LIST.length;
+    console.log("Change next image", nextIndex);
+    nextImg.src = getGameImage(nextIndex);
+    nextImg.style.transition = "";
+    nextImg.style.transform = `translateX(100%)`;
+    curIndex = nextIndex;
+    swapTm = setTimeout(() => {
+        console.log("Swap images");
+        setActivePoint(curIndex);
+        changeActiveImage(curImg, nextImg);
+    }, 3000);
+}
+
+function changeActiveImage(curImg, nextImg) {
+    const TRANSITION_SEC = 1;
+    curImg.style.transform = `translateX(-100%)`;
+    curImg.style.transition = `${TRANSITION_SEC}s`;
+    curImg.style.zIndex = 2;
+    // curImg.style.transitionTimingFunction = "linear";
+
+    nextImg.style.transform = `translateX(0)`;
+    nextImg.style.transition = `${TRANSITION_SEC}s`;
+    nextImg.style.zIndex = 1;
+    // nextImg.style.transitionTimingFunction = "ease-out";
+
+    nextImg.dataset.active = true;
+    curImg.dataset.active = false;
+
+    curImg.ontransitionend = () => {
+        curImg.ontransitionend = null;
+        console.log("ontransitionend");
+        prepareNextImage(nextImg, curImg);
+    };
+}
+
+function setSliderActiveIndex(index) {
+    console.log("Set slider active index", index);
+    setActivePoint(index);
+    clearTimeout(swapTm);
+
+    const img1 = document.getElementById("slider-img-1");
+    const img2 = document.getElementById("slider-img-2");
+
+    img1.ontransitionend = null;
+    img2.ontransitionend = null;
+
+    const curImg = img1.dataset.active ? img1 : img2;
+    curImg.src = getGameImage(index);
+    curImg.style.transform = "";
+    curImg.style.transition = "";
+    curImg.style.zIndex = 3;
+}
